ee60a8b7b1ae99f1183934a8fb760bec4316e2bf
muradbayramovcode <yeraznefesi@gmail.com>
2024-04-30 16:40:00 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// Daxil edilen metnin uzunlugunu gosterin console.log'da 
+
+// let metin = prompt("Metni daxil edin");
+// console.log(metin.length);
+
+
+
+// Duzbucagin sahesi
+
+// let a = Number (prompt("Sol kenarini daxil edin"));
+// let b = Number (prompt("Ust kenarini daxil edin"));
+// let sahe = (a * b);
+// alert(sahe);
+
+
+
+//Prompt methodundan istifade ederek ucbucagin sahesini hesablayin (Heron dusturu ile)
+
+// let x = Number (prompt("Birinci kenarını daxil edin"));
+// let y = Number (prompt("İkinci kenarını daxil edin"));
+// let z = Number (prompt("Üçüncü kenarını daxil edin"));
+// let halfperimeter = (x + y + z) / 2;
+// let sahe = Math.sqrt((halfperimeter*(halfperimeter-x)*(halfperimeter-y)*(halfperimeter-z)));
+// alert(sahe);
